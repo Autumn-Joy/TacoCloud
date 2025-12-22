4384651f2eeae5f99da03214da78b902bc9a07b1
Autumn Hicks <ajhicks1511@gmail.com>
2025-12-22 11:57:04 -0500

    feat(db): add id and date fields to taco and tacoOrder
    - prep for db

--- src/main/java/com/example/tacocloud/domain/Taco.java
+++ src/main/java/com/example/tacocloud/domain/Taco.java
@@ -1,10 +1,14 @@
 package com.example.tacocloud.domain;
 
+import java.util.Date;
 import java.util.List;
 import lombok.Data;
 
 @Data
 public class Taco {
+
+    private Long id;
+    private Date createdAt =  new Date();
     private String name;
     private List<Ingredient> ingredients;
 }
