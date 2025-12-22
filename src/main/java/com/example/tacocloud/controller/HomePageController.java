7b21b504e44890afb9d9a3d937bcedd3515c20e5
Autumn Hicks <ajhicks1511@gmail.com>
2025-12-22 11:57:53 -0500

    docs: add refactor note to HomePageController

--- src/main/java/com/example/tacocloud/controller/HomePageController.java
+++ src/main/java/com/example/tacocloud/controller/HomePageController.java
@@ -12,3 +12,22 @@ public class HomePageController {
     }
 
 }
+
+// revisit section 2.5 for refactoring this controller to use `WebMvcConfigurer` instead
+
+/*
+ *
+ *
+ * By extending an existing configuration class,
+ * you can avoid creating a new configuration class,
+ * keeping your project artifact count down.
+ *
+ *
+ * on the other hand
+ * creating a new configuration class
+ * for each kind of configuration (web, data, security, and so on),
+ * can keep the application bootstrap configuration clean and simple.
+ *
+ *
+ */
+
