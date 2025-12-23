872d9f048fca566d48a9d928435a1b26343851dc
Autumn Hicks <ajhicks1511@gmail.com>
2025-12-23 14:13:20 -0500

    feat(db): refactor to use crudRepository
    - IngredientRepository
    - OrderRepository
    
    - remove obsolete Jdbc implementation versions

--- src/main/java/com/example/tacocloud/repository/IngredientRepository.java
+++ src/main/java/com/example/tacocloud/repository/IngredientRepository.java
@@ -22,12 +22,10 @@
 package com.example.tacocloud.repository;
 
 import com.example.tacocloud.domain.Ingredient;
-import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
 
-public interface IngredientRepository {
-    Iterable<Ingredient> findAll();
-    Optional<Ingredient> findById(String id);
-    Ingredient save(Ingredient ingredient);
+public interface IngredientRepository extends CrudRepository<Ingredient, String> {
+
 }
 
 
