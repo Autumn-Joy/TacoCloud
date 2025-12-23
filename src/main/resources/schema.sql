ba5bd049bfad4136baa8cad555d901e05581adf4
Autumn Hicks <ajhicks1511@gmail.com>
2025-12-23 13:40:05 -0500

    fix(db): schema adjustment for tables `taco` and `ingredient_ref`

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,10 +11,13 @@ create table if not exists taco_order
     cc_cvv          varchar(3)  not null,
     placed_at       timestamp   not null
 );
+
+drop table taco CASCADE;
 create table if not exists taco
 (
     id             bigint primary key,
     name           varchar(50) not null,
+    taco_order     bigint      not null,
     taco_order_key bigint      not null,
     created_at     timestamp   not null,
     foreign key (taco_order_key) references taco_order(id)
@@ -27,10 +30,12 @@ create table if not exists ingredient
     type varchar(10) not null
 );
 
+drop table ingredient_ref;
 create table if not exists ingredient_ref
 (
     ingredient varchar(4) not null,
-    taco_id    bigint     not null,
+    taco       bigint     not null,
+    taco_key   bigint     not null,
     foreign key (ingredient) references ingredient(id),
-    foreign key (taco_id) references taco(id)
+    foreign key (taco) references taco(id)
 );
