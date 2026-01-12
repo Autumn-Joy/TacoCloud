3410bdca20c1a4a7130932f5542cb6ba8ef72502
Autumn Hicks <ajhicks1511@gmail.com>
2026-01-12 11:52:30 -0500

    chore: clean up imports in `User`

--- src/main/java/com/example/tacocloud/domain/User.java
+++ src/main/java/com/example/tacocloud/domain/User.java
@@ -1,19 +1,22 @@
 package com.example.tacocloud.domain;
 
+import java.util.Arrays;
+import java.util.Collection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.Arrays;
-import java.util.Collection;
+
 @Entity
 @Data
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
