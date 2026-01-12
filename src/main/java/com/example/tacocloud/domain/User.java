6c7a1a57993d4ef19258defe62f922db1fcbf01d
Autumn Hicks <ajhicks1511@gmail.com>
2026-01-12 12:38:39 -0500

    feat(auth): update `User` entity to include email field

--- src/main/java/com/example/tacocloud/domain/User.java
+++ src/main/java/com/example/tacocloud/domain/User.java
@@ -31,6 +31,8 @@ public class User implements UserDetails {
 
     private final String username;
     private final String password;
+    private final String email;
+
     private final String fullname;
     private final String street;
     private final String city;
